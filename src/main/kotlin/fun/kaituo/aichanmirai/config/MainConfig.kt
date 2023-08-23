1c7ec93ae366dce45ba6cfb9e114491aa33b0610
MSDNicrosoft <wang3311835119@hotmail.com>
2023-08-24 04:49:29 +0800

    style: Fix detekt
    Signed-off-by: MSDNicrosoft <wang3311835119@hotmail.com>

--- src/main/kotlin/fun/kaituo/aichanmirai/config/MainConfig.kt
+++ src/main/kotlin/fun/kaituo/aichanmirai/config/MainConfig.kt
@@ -30,10 +30,10 @@ object MainConfig : AutoSavePluginConfig("MainConfig") {
     val bindPort by value<Int>(30300)
 
     @ValueDescription("客户端认证的密码")
-    val token : MutableList<String> by value()
+    val token: MutableList<String> by value()
 
     fun genKey() {
-        val keyString : String = Key.generateKey().serialise()
+        val keyString: String = Key.generateKey().serialise()
         if (token.isEmpty()) {
             token.add(0, keyString)
         } else {
