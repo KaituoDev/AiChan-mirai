9124deaf53640d333b4e3d880969f95006cbdaf3
MSDNicrosoft <wang3311835119@hotmail.com>
2023-08-24 04:24:49 +0800

    build: Add `Detekt` & Optimize Gradle script
    Signed-off-by: MSDNicrosoft <wang3311835119@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,17 @@
-//import io.gitlab.arturbosch.detekt.Detekt
-//import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 
-//val kotlinVersion: String by project
 
 plugins {
     val kotlinVersion = "1.9.0"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.serialization") version kotlinVersion
+    kotlin("jvm") version (kotlinVersion)
+    kotlin("plugin.serialization") version (kotlinVersion)
 
-    id("net.mamoe.mirai-console") version "2.13.2"
-//    id("io.gitlab.arturbosch.detekt") version("1.23.1")
+    val detektVersion = "1.23.1"
+    id("io.gitlab.arturbosch.detekt") version (detektVersion)
+
+    id("net.mamoe.mirai-console") version ("2.15.0")
+    id("com.github.johnrengelman.shadow") version ("8.1.1")  // FIXME
 }
 
 group = "fun.kaituo.aichanmirai"
@@ -24,37 +26,33 @@ repositories {
 
 dependencies {
     // https://mvnrepository.com/artifact/org.xsocket/xSocket
-    implementation("org.xsocket:xSocket:2.8.15")
+    implementation("org.xsocket:xSocket:${property("xsocket_version")}")
     // https://mvnrepository.com/artifact/com.alibaba/fastjson
-    implementation("com.alibaba:fastjson:2.0.39")
+    implementation("com.alibaba:fastjson:${property("fastjson_version")}")
     // https://mvnrepository.com/artifact/com.macasaet.fernet/fernet-java8
-    implementation("com.macasaet.fernet:fernet-java8:1.5.0")
+    implementation("com.macasaet.fernet:fernet-java8:${property("fernet_version")}")
 
-    val detekt = "1.23.1"
-//    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detekt")
+    val detektVersion = "1.23.1"
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
 
 }
 
 mirai {
     jvmTarget = JavaVersion.VERSION_17
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+
+detekt {
+    toolVersion = "1.23.1"
+    parallel = true
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    autoCorrect = false
 }
 
-//detekt {
-//    toolVersion = "1.23.1"
-//    parallel = true
-//    config.setFrom(file("config/detekt/detekt.yml"))
-//    buildUponDefaultConfig = true
-//    autoCorrect = false
-//}
-//
-//tasks.withType<Detekt>().configureEach {
-//    jvmTarget = "17"
-//}
-//
-//tasks.withType<DetektCreateBaselineTask>().configureEach {
-//    jvmTarget = "17"
-//}
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = "17"
+}
+
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+    jvmTarget = "17"
+}
